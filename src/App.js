5aee3452ec6f6be3ae1e9080af0e6f3a12b8909b
Thanh-Nguyen <nguyetha0725@gmail.com>
2022-12-27 15:01:09 -0600

    Add Meal component and render to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Header from './components/Layout/Header';
-import { Fragment } from 'react';
+import Header from "./components/Layout/Header";
+import { Fragment } from "react";
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
