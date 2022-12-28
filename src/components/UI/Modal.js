14dbf41797f690db2bb3de6b5b87bee77b914581
Thanh-Nguyen <nguyetha0725@gmail.com>
2022-12-28 10:20:03 -0600

    Update modal component

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,7 +1,28 @@
-import classes from './Modal.module.css';
+import { Fragment } from "react";
+import { ReactDOM } from "react-dom";
+import classes from "./Modal.module.css";
 
-const Modal = props => {
-    
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<ModalOverlay />, portalElement)}
+    </Fragment>
+  );
 };
 
 export default Modal;
